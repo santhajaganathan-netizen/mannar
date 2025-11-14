6aaf3e1ad41117fdf02a7561afbe4e691061a816
Ravi J <raviregi@gmail.com>
2025-11-14 21:58:00 +0530

    updated files

--- clean.sh
+++ clean.sh
@@ -2,8 +2,8 @@ dop container stop medusa-storefront || true
 dop container rm medusa-storefront || true
 dop container stop medusa-backend || true
 dop container rm medusa-backend || true
-dop container stop medusa-db || true
-dop container rm medusa-db || true
+dop container stop medusa_postgres || true
+dop container rm medusa_postgres || true
 docker rmi medusa-storefront-img || true
 docker rmi medusa-backend-img || true
 docker rmi medusa-db-img || true
