168aeccca1f678eb353d8f553506838b8b35081d
VM Win <raviregi@hotmail.com>
2025-11-14 17:03:06 +0530

    I have local script to handle docker task dop=Docker Operations

--- clean.sh
+++ clean.sh
@@ -1,9 +1,9 @@
-docker container stop medusa-storefront || true
-docker container rm medusa-storefront || true
-docker container stop medusa-backend || true
-docker container rm medusa-backend || true
-docker container stop medusa-db || true
-docker container rm medusa-db || true
+dop container stop medusa-storefront || true
+dop container rm medusa-storefront || true
+dop container stop medusa-backend || true
+dop container rm medusa-backend || true
+dop container stop medusa-db || true
+dop container rm medusa-db || true
 docker rmi medusa-storefront-img || true
 docker rmi medusa-backend-img || true
 docker rmi medusa-db-img || true
