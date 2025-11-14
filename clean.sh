d27d07591458456fc5c89c0a6b8df218e479f906
Ravi J <raviregi@gmail.com>
2025-11-14 16:46:40 +0530

    updated clean.sh with store container & image

--- clean.sh
+++ clean.sh
@@ -1,6 +1,9 @@
-dop container stop medusa-db
-dop container rm medusa-db
+dop container stop medusa-store
+dop container rm medusa-store
 dop container stop medusa-backend
 dop container rm medusa-backend
-docker rmi medusa-db-img
+dop container stop medusa-db
+dop container rm medusa-db
+docker rmi medusa-store-img
 docker rmi medusa-backend-img
+docker rmi medusa-db-img
